ea6d5ae1c79deaefd5f570e0d6ab0d358afc7bb0
ERPlegend <103886584+reefer42@users.noreply.github.com>
2024-03-08 14:58:47 -0600

    Add files via upload

---
+++ scriptX.sh
@@ -0,0 +1,283 @@
+
+
+
+for i in {1..10}; do
+  random_color=$(shuf -i 1-255 -n 1)
+  printf "\e[38;5;${random_color}m\e[1mXkaliber os team presents\e[0m\n"
+  sleep 0.1
+done
+
+for i in {1..10}; do   random_color=$(shuf -i 1-255 -n 1);   printf "\e[38;5;${random_color}m\e[1mXkaliber os team present⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⡿⣽⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣳⢯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣟⣾⢭⣛⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡡⣱⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣟⣿⣽⣿⢿⣎⠷⣭⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢎⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡽⣾⣻⢿⣟⣾⡹⢖⣻⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡫⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣽⣫⢟⡷⢯⣟⣯⣷⡙⢎⡵⢻⡾⣽⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣳⣟⡾⣽⣻⣞⣷⡽⣜⠢⢞⣽⣹⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣛⣛⣛⣛⣛⣛⡛⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣾⣽⣮⠵⢮⡜⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣭⣉⡛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣱⣼⣎⣿⣿⣽⣻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣉⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣌⠻⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣟⡿⣯⢟⡏⠡⠌⣛⢾⣻⣟⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⡉⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣷⡘⢿⣿⣎⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⡻⣜⣏⡖⠡⠘⢌⡳⢯⣟⡿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡠⢙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣷⡝⣧⠻⣿⡆⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣞⡳⣝⢦⡝⣂⠁⢎⡵⢯⡾⣽⣿⡿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣍⢿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⢝⣻⠿⠇⣭⢧⡙⣷⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡟⡵⢫⡜⢎⠾⡰⣘⢮⣽⣫⣿⢿⣹⡽⣯⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣽⣟⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡏⣾⣿⣿⣿⣿⣿⡿⠿⠷⠉⠉⣁⠩⣘⡳⣿⣷⣗⠪⣀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣽⡳⢧⡜⣎⡱⢳⣞⣿⡾⣷⢯⣟⣧⢿⣻⣽
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣯⣿⣿⣿⣿⣿⣟⣯⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣫⡇⣿⣿⣿⣿⣿⣿⡇⣰⢶⣲⣷⢿⣮⣽⣟⢮⣝⣿⢷⣬⣀⠙⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡿⣯⣜⢶⣩⢷⣞⡷⣿⣟⡿⣾⣹⢯⣷⣻
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⢯⣿⣿⣿⣟⣯⣿⢾⣽⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣷⡷⡇⣻⣿⣿⣿⣿⣿⣿⣯⣾⣿⣾⣿⡿⣯⣟⣷⡮⢭⣟⠛⢿⣦⡈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣯⣫⢼⣽⣻⢾⣝⡶⢯⣟⡶⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣋⣾⣿⣿⣻⡾⣽⣟⣾⣟⣿⣾⣿⢿⣽⣻⣾⣿⣿⣿⣿⣟⣛⡇⢹⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣴⣴⣛⠻⣔⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣻⢾⣽⣻⢮⡝⣿⣞⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣴⣿⣿⣻⣞⣷⣻⢷⡻⣾⣽⣿⢯⡻⣯⢾⣿⣿⣿⣿⣿⣻⣯⠷⣆⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣾⡿⢄⡈⣙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢾⣽⣻⣾⣽⣾⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢃⣾⣿⣿⣿⣿⣿⠾⣝⢾⣽⡿⡝⢮⢳⣝⣳⢯⣿⣿⣿⣿⣿⣻⢿⡸⢍⡆⠋⣿⡿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡝⣿⣟⣿⣷⣯⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣡⣿⣿⣿⣿⣿⣿⢯⡻⣼⡿⡫⢜⡙⣎⢳⢮⡝⣯⣿⣿⣿⣿⠘⢿⣿⣿⣧⠿⡀⣌⣃⣥⣮⣿⣻⡿⠿⣿⣿⣿⡿⠋⣿⣿⣿⢽⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣴⣿⣿⣿⣿⣿⣿⣟⢣⡳⢏⠒⡥⢓⡜⣌⠳⣬⡹⣧⣿⣿⣿⣿⢔⢄⣭⣕⣚⣯⣽⣟⣿⡾⣿⣿⣷⣿⣿⣗⣲⡤⣔⣸⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢏⣾⣿⣿⣿⣿⣿⣿⡟⠤⣛⠱⢊⠕⡘⠦⡜⣌⠳⣥⣛⠶⣿⣿⣿⡟⣴⣿⣿⣿⣉⡭⣅⠘⣿⣾⠺⣻⡟⠁⣼⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣾⣿⣿⣿⣿⣿⣿⣿⢁⠟⣠⣩⣬⣼⣴⣧⣼⣴⡫⢴⢹⢌⢿⣻⣽⣷⣿⣿⣿⣿⣷⣾⡬⢆⢸⣽⢿⣿⠃⢸⣿⣿⣿⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢃⣺⣾⣿⣿⣿⣿⣿⣿⣿⣿⣷⢩⠞⡘⢾⡿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⢿⣯⡿⠠⠹⠻⣿⣿⣿⣿⣿⣿⣿⣯⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⡉⢾⡽⣞⣯⡿⣿⢿⡿⢻⣿⣿⠟⢃⢸⣷⡇⠘⢄⡀⠈⠈⢿⣿⣿⣿⣿⡿⠈⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠘⣿⢺⡝⣿⣭⣿⠁⣾⡿⠋⣴⣿⠘⣷⡇⡘⠀⢉⠢⣥⠀⡙⣿⣟⡋⠁⢠⣿⣿⣿⣿⣿⣿⡿⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣿⣿⣶⣦⢛⢿⢟⣽⣿⣿⢟⣵⣿⣿⣿⣿⡇⣧⣏⠼⣩⢿⡆⡀⠉⢠⣾⣿⣿⠐⣧⠁⣿⣶⣄⡂⠌⢷⣴⣘⣿⡇⠀⢸⣿⣿⣿⣿⣿⢟⣱⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⢹⢸⣾⣿⡿⣣⣿⣿⣿⣿⣿⡿⠁⣿⣿⣿⣤⣛⡇⡄⢰⠣⣜⢿⣿⡇⡿⠀⣿⣽⡟⠛⣮⣴⣩⢷⣿⡇⠀⢸⣿⣽⣻⠟⣡⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡘⣿⣿⡟⣰⣿⣿⣿⣛⢿⣿⢸⢸⣿⠟⣴⣿⣿⣿⣿⣿⣿⣧⠀⣿⣿⣿⣿⣽⡇⠃⢰⣬⣐⠎⣝⡇⡅⠀⣿⣿⡇⠀⣿⣿⣷⣯⣿⡇⠀⢸⣿⡿⢋⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣾⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡘⡟⣰⣿⣿⢳⣿⣿⣿⣿⢸⠸⣫⡞⣿⣿⣿⣿⣿⣿⡇⣿⡄⠸⣿⣷⣾⣿⡇⡃⢸⡧⢭⠱⣢⡇⡏⠀⣿⣿⡇⠀⣿⣿⣿⣿⣿⡇⠀⢸⣟⣷⡟⣿⣿⣛⠻⣿⣿⣷⠀⣰⣶⣶⣌⠻⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⡘⣿⣿⣿⣧⣛⣿⣞⣿⢸⢰⣿⣿⠹⣿⣿⣿⣿⣿⣿⣿⣧⠀⢿⣻⣿⣿⡇⠇⢸⣷⣏⡧⢥⡇⡳⠀⣿⣿⡇⠐⣛⠿⣿⣿⣿⡇⠀⢘⢻⣿⡧⢿⣿⣿⣶⣿⣿⣟⠀⣿⣿⣿⣿⣠⣿⣿⣿⣿⣿⣿⣻⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢋⣼⣷⡘⣿⣿⢿⣿⣿⣿⣿⠸⢸⣿⡿⢧⢻⣿⣿⣿⢻⣿⣯⣽⡄⠸⣿⣿⣿⡏⡄⢸⣿⣿⣿⣯⡇⡇⠀⣿⣿⡇⠀⣿⣿⣆⠹⣿⡇⠀⢸⡄⢿⡇⣿⣿⣿⣿⣿⣿⣯⠀⣿⠻⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣾⣿⣿⡷⣸⣭⣻⣾⣻⣯⣿⢱⢸⣿⣿⣯⠊⢿⣿⡟⣼⣿⣿⢯⣷⠀⢿⣿⢿⣗⠃⢸⣿⣿⣿⢻⣇⡇⠀⣿⣿⡇⠀⣿⣿⣿⣲⣿⡇⠀⢸⡇⢸⡇⣿⣿⣿⣃⣿⢿⡷⠀⣿⡆⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢹⣿⢿⣷⣿⢿⣿⣿⠬⢸⣿⣽⣿⣷⡈⢿⣵⣿⣿⢞⡞⢹⣦⣬⣯⣾⣯⠀⣼⣿⣿⣧⣬⡵⡇⠀⣿⣿⣦⣤⣿⣭⣿⣿⣿⡇⠀⢸⣷⠀⣿⢿⣿⣿⣿⣿⣿⡗⠀⢿⣿⡌⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣏⣿⣿⣮⣏⡿⣾⣿⢈⢸⣿⣿⢿⣿⣷⡈⢿⣿⣟⣪⠇⠘⣿⣿⣿⣿⣷⣆⢿⣿⣿⣿⣿⡗⡟⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣧⠘⣾⣿⣷⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⢯⣟⣿⣷⣝⡿⣾⢿⡿⣯⣶⣦⣿⣿⣿⣻⡿⣷⡈⢻⡽⣐⠛⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣧⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⣼⣿⣿⣧⡈⢿⣿⣿⣿⣿⣿⣯⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡾⣟⡿⣽⡾⣿⣿⣿⣿⣟⢿⡹⢯⣟⣯⣿⢳⡿⣹⣷⡀⢻⡖⢼⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡧⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾⣿⣿⣿⣿⣿⡄⢿⣿⣿⣿⣿⡿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡽⣳⠿⣽⣿⣿⣿⣟⢮⣝⡳⢞⡼⣣⢟⡼⣳⠏⣷⡀⢻⣨⠀⠸⣿⣛⣵⣿⣿⣿⣿⣿⣿⣿⡇⣟⣷⣿⣿⣿⣿⣿⣿⣿⢏⣴⣿⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣝⣿⣿⣿⣿⣿⣿⣹⠲⣝⠫⡖⡝⣎⠷⣿⣾⣻⣿⣄⠛⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣯⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠳⣌⠳⡘⢦⡑⠞⣬⢙⢿⣿⣿⣦⡀⡀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⡇⡷⣟⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠱⣌⢣⡙⢦⡙⠞⣤⢫⠾⣟⣿⣿⣿⣦⣀⡀⠀⠙⠻⢿⣿⣿⣿⣿⣯⣟⣧⣿⣭⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠌⢦⠙⡤⡙⣎⠶⣩⣾⣴⣿⣿⣿⣿⣿⣿⣿⣿⢏⡙⠿⣿⣿⣿⡷⣟⣾⣿⣿⢼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⡟⣦⡩⢰⢱⢊⡵⣯⢷⣯⡿⣽⣿⣿⡿⣯⣟⡿⣷⣜⠢⣝⠛⣿⡟⣧⢿⣿⣿⣻⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣽⣟⡿⣿⣿⢧⣛⣬⢻⣄⣇⢻⣜⡯⣷⢯⣟⡷⣯⡿⣿⡟⢿⣯⠻⢿⢏⡺⠷⡄⢘⣧⢯⠘⠟⣛⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⢯⣟⡿⣾⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⡳⢯⣟⣿⣽⣿⡜⡲⢭⢲⡎⡿⣜⣳⣯⠷⣷⡪⢋⢈⣠⣬⢴⠒⣎⣓⣓⣲⣖⣏⡮⣗⣻⣻⣞⣢⣽⣹⢩⢛⡚⢿⡿⣿⣿⣿⣿⣿⢿⣿⣻⢾⣽⣛⡾⣽⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣷⡿⣿⣿⣿⣿⣿⣏⡿⣽⣻⢾⣽⣳⢭⡓⡧⣝⢧⣛⢶⣯⣷⡷⢘⣮⣷⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⡗⣯⢽⣿⣿⣿⣿⣿⣷⡳⣍⡞⣦⠹⣿⣿⡿⣭⢳⡞⣭⡟⣞⣧⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣽⡾⣽⣻⣽⡿⣿⣿⣿⣷⢣⣟⣯⡷⣯⡷⣙⠾⣭⣛⣯⣿⣽⣟⣶⣹⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⣷⢻⣿⣿⣿⣿⣿⣿⣿⣽⣳⣯⢿⣜⡟⣾⡱⣏⠾⣵⢫⢿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣽⣳⣟⡾⣽⡿⣷⢿⡽⣗⢮⢷⣻⢷⣻⢌⡿⣜⣳⢞⣼⣿⣾⣿⣿⣷⣿⣿⣿⢟⠻⣟⢋⡛⡹⡋⢏⠣⢱⡏⡹⡿⡛⠿⣿⣿⣿⣿⣟⣾⣿⢿⣿⡾⡱⢏⡻⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣳⣯⣟⣷⣻⢯⡿⣝⡯⡞⣭⢏⡿⣽⣞⣳⡽⣎⠿⣜⣿⣿⣟⣿⣿⣿⣞⣯⢾⣘⡆⢣⢌⡱⠌⡆⠇⡸⣇⠱⡥⣙⢮⡹⢽⣿⣿⣿⣿⣿⣿⣿⣷⢯⣫⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⡽⣫⣟⠾⣝⡾⣙⢖⣫⢽⣳⢯⣗⣻⣜⡻⣜⡹⣿⣿⣿⣿⣿⣿⣏⣾⣿⣽⡟⣶⣥⣋⣔⠃⠷⡌⠳⡌⡵⢢⣽⣿⣿⣿⣿⣿⣿⣿⣿⡿⢧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣭⣟⢧⣏⡹⢦⡝⣮⢳⢏⡾⣱⢎⠷⣜⡳⡽⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣼⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣲⡙⡦⡝⣎⠻⣎⢷⡹⢎⣛⡘⣓⣉⠓⡫⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⡇⠐⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣝⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢣⡛⡴⠹⣌⠳⣍⠶⣩⢛⡜⣳⣓⣮⣻⣹⢏⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣙⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⣫⣜⠱⡹⣌⠳⣌⠲⢡⢎⡴⢏⡾⢵⡳⢯⣟⡾⣻⢯⣟⣿⣿⢿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⠿⣏⡳⣝⣎⢷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣷⣳⢮⣹⠱⣎⠵⣈⢇⠧⣞⣭⢻⣜⢯⣽⡻⣾⡽⣿⣟⣻⣻⣼⣿⣳⢯⡟⣭⡇⠀⢸⣿⢿⡻⣜⢫⢎⡽⡸⢵⣒⢮⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⣿⣾⣷⣾⣵⣣⢎⢿⣸⣜⡳⢎⡷⣖⢷⢧⡟⡷⢯⣿⣿⣿⣳⣟⣯⡽⣲⡇⠀⢸⡭⢲⡝⣬⠳⣮⢕⡯⢳⡭⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡵⣞⣞⡺⣖⢮⣝⡻⡼⠽⢮⡭⣽⣝⡿⣿⠽⣎⠷⣩⢶⡹⣥⢇⠀⡞⣜⢣⢞⣥⠻⣴⢫⡞⣧⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢽⣚⣳⡞⣵⢫⠟⣬⢳⣝⢿⣙⡞⣭⢾⡹⢧⡻⣜⡳⢭⢶⣙⢦⣻⠀⡟⣜⢮⣓⢮⡻⣴⣿⣾⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⡯⡗⣯⠷⡽⢭⢫⡻⣵⢛⡞⣳⢚⡵⢪⠶⣙⠧⡳⢌⣳⡙⢮⣙⢦⡳⢸⡱⣏⣾⣽⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢟⢏⣸⣽⣻⠽⣶⡭⣹⣭⢟⡭⢏⢧⢓⡆⡳⡜⢣⢍⡞⣥⢛⡌⢧⠓⣍⠶⣩⢗⢮⡳⣝⢧⣛⣾⡽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣾⣾⣵⣯⣍⣳⢨⡹⢱⡎⡷⢪⡝⢢⢇⡜⡱⣜⢣⡞⡜⣆⠏⡜⣡⣉⠦⣛⡜⣮⢳⡽⣞⣯⢿⣳⡿⣽⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣵⡻⣝⠳⣜⢣⠞⡼⣑⢎⡳⡜⡱⡜⡸⣔⢣⡜⣣⢧⣛⢶⢫⢞⡽⣞⡿⣽⣻⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣷⠻⣜⠫⡜⢥⠛⡰⡡⢎⡱⢎⡵⢱⠳⣬⣓⢾⡱⢯⣞⣭⢳⣫⢞⡵⣏⡷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣾⣹⢺⢅⡛⢤⢋⠔⡱⢌⣒⢣⡚⣥⠻⣴⡹⢮⣽⣻⢾⣭⡷⣯⢾⡽⣽⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢳⡭⣓⠮⡜⡡⢎⡜⠤⢣⠜⣢⡙⢦⡛⠶⣭⢳⣯⢿⣿⣿⣿⣟⣯⡿⣷⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡩⠞⡰⡑⠦⠸⡌⡅⣎⠱⡜⣣⢝⡻⡜⡿⣽⣿⣿⣿⣿⣿⡿⣽⣯⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠳⣱⢉⠲⣑⠜⡰⢎⡳⣙⠴⣋⡶⣽⣻⢷⣻⣟⡿⣿⡿⣽⣟⣯⣿⡿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣷⣍⠦⡉⢖⡡⢎⠱⢎⣵⣩⢞⡧⣟⣳⡽⣯⠷⣯⣟⢷⡻⢧⣟⢾⣳⢿⡽⣻⢾⣽⣻⣟⡿⣿⣯⣷⢿⣽⣻⡿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿s\e[0m\n";   sleep 0.1; done
+
+
+sudo echo "Xkaliber os team presents"
+sudo echo "Script X"
+#deps
+sudo apt update && sudo apt install python3 git gettext libappstream-dev libflatpak-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libhandy-1-dev libjson-glib-dev libpackagekit-glib2-dev libpolkit-gobject-1-dev libsoup2.4-dev libxml2-dev libxml2-utils meson valac curl wget dnsmasq konsole aptitude sddm dnsmasq plasma-mobile -y
+
+sudo aptitude install plasma-discover -y
+
+sudo apt install gnome-software-plugin-flatpak -y
+
+sudo apt-get update --allow-unauthenticated
+
+sudo dpkg --add-architecture i386
+apt install libgl1-nvidia-glvnd-glx:i386
+#dns
+read -p "Do you want to setup a Dns cache server? this will significantly increase your internet speed and lower lantencys (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    sudo apt update
+    sudo echo "server=8.8.8.8" | sudo tee -a /etc/dnsmasq.conf
+    sudo echo "server=8.8.4.4" | sudo tee -a /etc/dnsmasq.conf
+
+    # Add Cloudflare's DNS servers
+    sudo echo "server=1.1.1.1" | sudo tee -a /etc/dnsmasq.conf
+    sudo echo "server=1.0.0.1" | sudo tee -a /etc/dnsmasq.conf
+
+   # Set the DNS cache size to 5000 entries
+   sudo echo "cache-size=5000" | sudo tee -a /etc/dnsmasq.conf
+
+   # Set the minimum cache time to live (TTL) to 86,400 seconds (1 day)
+   sudo echo "min-cache-ttl=86400" | sudo tee -a /etc/dnsmasq.conf
+
+   # Set the maximum cache TTL to 604,800 seconds (7 days)
+   sudo echo "max-cache-ttl=604800" | sudo tee -a /etc/dnsmasq.conf
+
+   # Instruct dnsmasq to ignore the /etc/resolv.conf file and use its own internal list of DNS servers
+   sudo echo "no-resolv" | sudo tee -a /etc/dnsmasq.conf
+
+   # Set the negative TTL to 60 seconds. This means that if dnsmasq receives a negative response from a DNS server, it will cache that response for 60 seconds before querying the server again.
+   sudo echo "neg-ttl=60" | sudo tee -a /etc/dnsmasq.conf
+
+    echo "Skipping dnsmasq."
+fi
+
+
+
+
+sudo systemctl enable dnsmasq
+
+
+
+#zram
+read -p "Do you want to zram?  (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    sudo apt install systemd-zram-generator -y
+
+    # Add commands to install the drivers from the downloaded deb file
+else
+    echo "Skipping Zram."
+fi
+
+# ... (continue with the rest of your script)
+
+
+echo "install lib5 (needed)"
+   sudo wget http://ftp.de.debian.org/debian/pool/main/n/ncurses/libtinfo5_6.4-4_amd64.deb
+   sudo chmod +x libtinfo5_6.4-4_amd64.deb
+   sudo dpkg -i libtinfo5_6.4-4_amd64.deb
+
+
+
+
+#pop shop
+#read -p "Do you want to install pop shop? (y/n): " answer
+
+#if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    #sudo git clone https://github.com/pop-os/shop.git
+    #cd shop
+    #sudo chmod 777 *
+    #sudo meson build --prefix=/usr
+   # cd build
+  #  sudo chmod 777 *
+ #   sudo ninja
+#    sudo ninja install
+    
+    # Add commands to install the drivers from the downloaded deb file
+#else
+ #   echo "Skipping pop shop."
+#fi
+
+#waydroid
+read -p "Do you want to install waydroid? (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    sudo apt install curl ca-certificates -y
+    sudo curl https://repo.waydro.id | sudo bash
+    sudo apt install waydroid -y
+   
+    sudo systemctl enable --now waydroid-container
+    
+    # Add commands to install the drivers from the downloaded deb file
+else
+    echo "Skipping waydroid."
+fi
+
+
+
+
+
+#gnome
+read -p "Do you want to install gnome? (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    sudo apt install gnome -y
+    # Add commands to install the drivers from the downloaded deb file
+else
+    echo "Skipping gnome."
+fi
+
+# ... (continue with the rest of your script)
+
+
+read -p "Do you want to install plasma-mobile? (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    sudo apt install plasma-mobile -y
+    # Add commands to install the drivers from the downloaded deb file
+else
+    echo "Skipping plasma."
+fi
+
+
+
+read -p "Do you want to install steam? (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    sudo wget http://media.steampowered.com/client/installer/steam.deb
+    sudo chmod +x steam.deb
+    sudo dpkg -i steam.deb
+    steam
+    # Add commands to install the drivers from the downloaded deb file
+else
+    echo "Skipping steam."
+fi
+
+
+read -p "Do you want to install nvidia drivers? (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    sudo wget https://developer.download.nvidia.com/compute/cuda/12.3.1/local_installers/cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
+   sudo chmod +x cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
+   sudo dpkg -i cuda-repo-debian12-12-3-local_12.3.1-545.23.08-1_amd64.deb
+   sudo cp /var/cuda-repo-debian12-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings
+   sudo add-apt-repository contrib
+   sudo apt-get update
+   sudo apt-get install -y cuda-toolkit-12-3 -y
+   sudo apt-get install -y cuda-drivers -y
+   
+    # Add commands to install the drivers from the downloaded deb file
+else
+    echo "Skipping nvidia."
+fi
+
+
+
+read -p "Do you want to install snap store? (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    sudo apt install snapd -y
+    sudo snap install hello-world
+    sudo snap install snap-store
+    
+    # Add commands to install the drivers from the downloaded deb file
+else
+    echo "Skipping snap."
+fi
+
+# AI Dependencies (optional)
+read -p "Do you want to install AI dependencies (like gpt)? (y/n) " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+  echo "**Warning:** Installing unofficial packages and dependencies may carry security risks. Consider using well-maintained repositories or building from trusted sources."
+
+  read -p "Continue with caution? (y/n) " answer2
+
+  if [[ "$answer2" =~ ^[Yy]$ ]]; then
+    echo "Installing AI dependencies..."
+
+    # Use package manager for installing Python and webkit2gtk
+    sudo apt update
+    sudo apt install python3-pip libwebkit2gtk-4.0-37
+
+    
+    sudo apt --fix-broken install
+    sudo apt install libwebkit2gtk-4.0-37
+    sudo wget https://github.com/lencx/ChatGPT/releases/download/v1.1.0/ChatGPT_1.1.0_linux_x86_64.deb
+
+sudo chmod +x ChatGPT_1.1.0_linux_x86_64.deb
+sudo dpkg -i ChatGPT_1.1.0_linux_x86_64.deb
+sudo apt install debgpt
+
+sudo echo "special thanks to ERPlegend && Jack"
